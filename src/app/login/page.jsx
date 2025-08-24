3641c13acc52235d68a8b6733636e519a683e8f0
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-08-24 14:56:30 +0300

    first commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,19 @@
+import { ModeToggle } from "@/components/ui/mode_toggle.jsx";
+
+ 
+const login=()=>{
+
+
+
+    return(
+        <div>
+ 
+            <div className="right-5  top-3 fixed">
+            <ModeToggle  />
+
+            </div>
+           
+        </div>
+    )
+}
+export  default login;
---
+++ src/components/ui/theme-provider.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { ThemeProvider } from "next-themes"
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
